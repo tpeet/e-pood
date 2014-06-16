3ded91679dafd4c2e913e89ee998bb2202a6a089
mare hunt <mare.hunt@netgroup.ee>
2014-06-16 12:37:45 +0300

    bugfix ie

--- Build/assets/js/scripts.js
+++ Build/assets/js/scripts.js
@@ -3,9 +3,6 @@ $(document).ready(function() {
 BindSlideToggle();
 cloneRightSideMainMenu();
 
-if (!Modernizr.svg) {
-  $(".navbar-brand img").attr("src", "images/elion-logo.png");
-}
 
   // CALL FRONTPAGE MORE
  
@@ -36,61 +33,9 @@ if (!Modernizr.svg) {
 });
 
 
-  // TAG FONT RESIZE
-  function TagsFontResize() {
-
-   if ($(".flashText-flash-normal").length) {
-    $(".flashText-flash-normal").bigtext({
-     maxfontsize: 22,
-     childSelector: '> .ee-big'
-    });
-   }
-
-   if ($(".flashText-ribbon").length) {
-    $(".flashText-ribbon").bigtext({
-     maxfontsize: 18
-    });
-   }
-  }
-
 $('.dropdown-menu').find('form').click(function (e) {
         e.stopPropagation();
       });
-
-// Alerts logic
-   $('#alertbtn').click(function() {
-      if ( !$('#alert1').is( '.in' ) ) {
-         $('#alert1').addClass('in');
-      }
-   });
-
-    $('#alertbtn2').click(function() {
-      if ( !$('#alert2').is( '.in' ) ) { 
-         $('#alert2').addClass('in');
-      }
-   });
-
-   $('#successbtn').click(function() {
-      if ( !$('#success1').is( '.in' ) ) {
-         $('#success1').addClass('in');
-
-         setTimeout(function() {
-            $('#success1').removeClass('in');
-         }, 3200);
-      }
-   });
-
-   $('#infobtn').click(function() {
-      if ( !$('#your-uniq-ID-123').is( '.in' ) ) {
-         $('#your-uniq-ID-123').addClass('in');
-
-         setTimeout(function() {
-            $('#your-uniq-ID-123').removeClass('in');
-         }, 4800);
-      }
-   });
-
-
 
 // Sliding commenting
 //*********************
@@ -119,66 +64,36 @@ function cloneRightSideMainMenu(){
     var _el = $('#collapsibleMainMenu-Pages').find('.navbar-nav').eq(1).find('li').eq(3);
 }
 
-(function (cash) {
- $.fn.textfill = function (options) {
-  return this.each(function () {
-   var text = $(this).html();
-   var oldFontSize = parseInt($(this).css("font-size"));
-   var contentFontSizes = [];
-   $(this).find('*').each(function (i, e) {
-    contentFontSizes[i] = parseInt($(this).css("font-size"));
-   });
-   $(this).html('');
-   var container = $('<span />').html(text).appendTo($(this));
-   var min = 1, max = 200, fontSize;
-   do {
-    fontSize = (max + min) / 2;
-    container.css('fontSize', fontSize);
-    container.find('*').each(function (i, e) {
-     $(this).css("font-size", fontSize);
-    });
-    var multiplier = $(this).height() / container.height();
-    if (multiplier == 1) { min = max = fontSize }
-    if (multiplier > 1) { min = fontSize }
-    if (multiplier < 1) { max = fontSize }
-   } while ((max - min) > 1);
-   fontSize = min;
-   coef = fontSize / oldFontSize;
-   if ($(this).width() < container.width()) {
-    min = 1;
-    do {
-     fontSize = (max + min) / 2;
-     container.css('fontSize', fontSize);
-     container.find('*').each(function (i, e) {
-      $(this).css("font-size", fontSize);
-     });
-     var multiplier = $(this).width() / container.width();
-     if (multiplier == 1) { min = max = fontSize }
-     if (multiplier > 1) { min = fontSize }
-     if (multiplier < 1) { max = fontSize }
-    } while ((max - min) > 1);
-    fontSize = min;
-    coef = fontSize / oldFontSize;
-   }
-   container.remove();
-   $(this).html(text);
-   var minFontSize = options.minFontPixels;
-   var maxFontSize = options.maxFontPixels;
-   var newFontSize = minFontSize && (minFontSize > fontSize) ?
-    minFontSize :
-      maxFontSize && (maxFontSize < fontSize) ?
-    maxFontSize :
-      fontSize;
+// counting rules for IE 8-9 as the limit is 4095
+// function countCSSRules() {
+//     var results = '',
+//         log = '';
+//     if (!document.styleSheets) {
+//         return;
+//     }
+//     for (var i = 0; i < document.styleSheets.length; i++) {
+//         countSheet(document.styleSheets[i]);
+//     }
+//     function countSheet(sheet) {
+//         var count = 0;
+//         if (sheet && sheet.cssRules) {
+//             for (var j = 0, l = sheet.cssRules.length; j < l; j++) {
+//                 if( !sheet.cssRules[j].selectorText ) {
+//                     continue;
+//                 }
+//                 count += sheet.cssRules[j].selectorText.split(',').length;
+//             }
 
-   coef = minFontSize && (minFontSize > fontSize) ?
-      minFontSize / oldFontSize :
-      maxFontSize && (maxFontSize < fontSize) ?
-      maxFontSize / oldFontSize :
-      coef;
-   $(this).find('*').each(function (i, e) {
-    $(this).css("font-size", newFontSize);
-   });
-   $(this).css('fontSize', newFontSize);
-  });
- };
-})(jQuery);
+//             log += '\nFile: ' + (sheet.href ? sheet.href : 'inline <style> tag');
+//             log += '\nRules: ' + sheet.cssRules.length;
+//             log += '\nSelectors: ' + count;
+//             log += '\n--------------------------';
+//             if (count >= 4096) {
+//                 results += '\n********************************\nWARNING:\n There are ' + count + ' CSS rules in the stylesheet ' + sheet.href + ' - IE will ignore the last ' + (count - 4096) + ' rules!\n';
+//             }
+//         }
+//     }
+//     console.log(log);
+//     console.log(results);
+// };
+// countCSSRules();
